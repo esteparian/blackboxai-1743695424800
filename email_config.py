cd8147ce8cbd0cd066b8631b0dbe016dd2551aef
esteparian <info.esteparia@gmail.com>
2025-04-03 22:01:35 -0300

    Add email_config.py 1743728493654

---
+++ email_config.py
@@ -0,0 +1,7 @@
+SMTP_CONFIG = {
+    'SERVER': 'smtp.gmail.com',
+    'PORT': 587,
+    'USERNAME': 'tu_email@gmail.com',
+    'PASSWORD': 'tu_password',
+    'FROM': 'no-reply@alertavecinos.com'
+}
